c8f5dea57e9848f50b78ce9f67916d8838d7f76e
Volko76 <70014984+Volko76@users.noreply.github.com>
2022-05-08 14:48:30 +0200

    add c# project

---
+++ AutoInstallCreoEtu/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.InfoLbl = new System.Windows.Forms.Label();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InfoLbl
+            // 
+            this.InfoLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InfoLbl.AutoSize = true;
+            this.InfoLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InfoLbl.Location = new System.Drawing.Point(20, 9);
+            this.InfoLbl.Name = "InfoLbl";
+            this.InfoLbl.Size = new System.Drawing.Size(771, 24);
+            this.InfoLbl.TabIndex = 1;
+            this.InfoLbl.Text = resources.GetString("InfoLbl.Text");
+            this.InfoLbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(106)))), ((int)(((byte)(117)))));
+            this.btnInstall.FlatAppearance.BorderSize = 0;
+            this.btnInstall.Location = new System.Drawing.Point(282, 61);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(246, 56);
+            this.btnInstall.TabIndex = 2;
+            this.btnInstall.Text = "Install Creo";
+            this.btnInstall.UseVisualStyleBackColor = false;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(184)))), ((int)(((byte)(65)))));
+            this.ClientSize = new System.Drawing.Size(804, 133);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.InfoLbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Automatic Install Creo Etu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label InfoLbl;
+        private System.Windows.Forms.Button btnInstall;
+    }
+}
+
---
+++ AutoInstallCreoEtu/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        private string link = "https://download.utc.fr/index.php/download/creo-v4-0/?wpdmdl=11";
+        private string folder_to_delete = "C:\\ProgramData\\PTC\\Licensing";
+        private string zip_folder = Path.GetFullPath(@"MED-100WIN-CD-410_M040_Win64.zip");
+        private string unzipped_folder = Path.GetFullPath(@".\UnzippedFolder");
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(zip_folder))
+            {
+                Directory.Delete(folder_to_delete, true);
+            }
+            if (Directory.Exists(unzipped_folder))
+            {
+                Directory.Delete(unzipped_folder, true);
+            }
+
+            //MessageBox.Show(zip_folder);
+            if (File.Exists(zip_folder))
+            {
+                ZipFile.ExtractToDirectory(zip_folder, unzipped_folder);
+            }
+            else
+            {
+                MessageBox.Show("Le zip ne se trouve pas dans le meme repertoire que le .exe ! Merci de le telecharger et de le mettre dans le meme repertoire avec le nom : MED-100WIN-CD-410_M040_Win64.zip");
+            }
+            Process.Start(unzipped_folder + "\\setup.exe", "readme.txt");
+            MessageBox.Show("Veuillez cliquer sur : Installer un nouveau logiciel puis Suivant");
+            MessageBox.Show("Acceptez le contract de license et l'accord d'exportation et appuyez sur suivant");
+            MessageBox.Show("Entrez la licence Entrer la licence BK410705EDSTUDENTUNICL dans le champ Product code");
+            MessageBox.Show("Cliquer ensuite sur Install licensing.");
+            MessageBox.Show("Connectez vous à votre compte PTC(si vous n'en avez pas crée, vous pouvez le créer ici : https://support.ptc.com/appserver/common/account/createAccount.jsp?tab=student) puis appuyez sur Login");
+            MessageBox.Show("Lorsque vous voyez le message Success! cliquez sur Next");
+            MessageBox.Show("Cliquez sur Install");
+            MessageBox.Show("L'installation va se lancer. Quand elle sera terminée appuyez sur Finish");
+            Environment.SetEnvironmentVariable("PTC_PMA_HC_URL_4", "https://documentations.utc.fr/creodoc/Creo4.0/help/creo_help_pma/");
+            if (Directory.Exists("C:\\Program Files\\PTC\\Creo 4.0\\M040\\Common Files\\formats"))
+            {
+                Directory.Delete("C:\\Program Files\\PTC\\Creo 4.0\\M040\\Common Files\\formats", true);
+            }
+            
+            Directory.Move("Installation creo 4.0_M040[4]\\formats", "C:\\Program Files\\PTC\\Creo 4.0\\M040\\Common Files\\formats");
+            if (File.Exists("C:\\Program Files\\PTC\\Creo 4.0\\M040\\Common Files\\text\\config.pro"))
+            {
+                File.Delete("C:\\Program Files\\PTC\\Creo 4.0\\M040\\Common Files\\text\\config.pro");
+            }
+            File.Move("Installation creo 4.0_M040[4]\\config.pro", "C:\\Program Files\\PTC\\Creo 4.0\\M040\\Common Files\\text\\config.pro");
+        
+        }
+    }
+}
